870ad712c99c95a1c18458cf1144184eabde5c6a
Marcos Santana <ssmarcosantana@gmail.com>
2022-08-30 17:45:46 -0300

    Iniciando o tratamento de excecoes

---
+++ Application/Program.java
@@ -0,0 +1,7 @@
+package Ex01.Application;
+
+public class Program {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Entities/Reservation.java
@@ -0,0 +1,48 @@
+package Entities;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Reservation() {
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public Integer duration() {
+        return 0;
+    }
+
+}
